859324efc33f4681467c6e1dbe54326e3dedf374
vlas <bashvlas@gmail.com>
2016-12-30 20:35:38 +0200

    backup

--- input/util.js
+++ input/util.js
@@ -1,20 +1,242 @@
 
 	window.x.util = ( function () {
-		
+
 		return {
 
 			text_to_json: function ( text ) {
-			
+
 				try {
-			
+
 					return JSON.parse( text );
-			
+
 				} catch ( e ) {
-			
-					return
-			
+
+					return undefined;
+
 				};
-			
+
+			},
+
+			text_to_doc: function ( text ) {
+
+				return m.parser.parseFromString( text, "text/html" );
+
+			},
+
+			html_to_doc: function ( html ) {
+
+				return m.parser.parseFromString( html, "text/html" );
+
+			},
+
+			doc_to_val: function ( rq ) {
+
+				var doc = rq[ 0 ];
+				var selector = rq[ 1 ];
+				var val_type = rq[ 2 ];
+				var detail = rq[ 3 ];
+
+				if ( val_type === "length" ) {
+
+					return doc.querySelectorAll( selector ).length;
+
+				} else {
+
+					var element = doc.querySelector( selector );
+
+					if ( element ) {
+
+						if ( val_type === "text" ) {
+
+							return element.innerText;
+
+						} else if ( val_type === "html" ) {
+
+							return element.innerHTML;
+
+						} else if ( val_type === "attr" ) {
+
+							return element.getAttribute( detail );
+
+						} else if ( val_type === "value" ) {
+
+							return element.value;
+
+						} else {
+
+							return null;
+
+						}
+
+					} else {
+
+						return null;
+
+					};
+
+				};
+
+			},
+
+			normalize_links: function ( element ) {
+
+				var link_arr = element.querySelectorAll( "a" );
+
+				for ( var i = link_arr.length; i--; ) {
+
+					link_arr[ i ].href = link_arr[ i ].href;
+
+				};
+
+				return element;
+
+			},
+
+			query: function ( rq ) {
+
+				if ( typeof rq.method === "undefined" ) {
+
+					var arr = rq[ 0 ];
+					var obj = rq[ 1 ];
+					var match_arr = [];
+
+					for ( var i = arr.length; i--; ) {
+
+						var match_flag = true;
+						var keys = Object.keys( obj );
+
+						for ( var j = keys.length; j--; ) {
+
+							var key = keys[ j ];
+
+							if ( obj[ key ] !== arr[ i ][ key ] ) {
+
+								match_flag = false;
+
+							};
+
+						};
+
+						if ( match_flag ) {
+
+							match_arr.push( arr[ i ] );
+
+						};
+
+					};
+
+					return match_arr;
+
+				} else if ( rq.method === "remove" ) {
+
+					for ( var i = rq.arr.length; i--; ) {
+
+						var item = rq.arr[ i ];
+						var match_bool = true;
+						var q_key_arr = Object.keys( rq.q || {} );
+						var nq_key_arr = Object.keys( rq.nq || {} );
+						
+						for ( var j = q_key_arr.length; j--; ) {
+
+							if ( item[ q_key_arr[ j ] ] !== rq.q[ q_key_arr[ j ] ] ) {
+
+								match_bool = false;
+
+							};
+
+						};
+						
+						for ( var j = nq_key_arr.length; j--; ) {
+
+							if ( item[ nq_key_arr[ j ] ] === rq.q[ nq_key_arr[ j ] ] ) {
+
+								match_bool = false;
+
+							};
+
+						};
+
+						if ( match_bool === true ) {
+
+							rq.arr.splice( i, 1 );
+
+						};
+
+					};
+
+				} else if ( rq.method === "find" ) {
+
+					var match_arr = [];
+
+					for ( var i = rq.arr.length; i--; ) {
+
+						var item = rq.arr[ i ];
+						var match_bool = true;
+						var q_key_arr = Object.keys( rq.q || {} );
+						var nq_key_arr = Object.keys( rq.nq || {} );
+						
+						for ( var j = q_key_arr.length; j--; ) {
+
+							if ( item[ q_key_arr[ j ] ] !== rq.q[ q_key_arr[ j ] ] ) {
+
+								match_bool = false;
+
+							};
+
+						};
+						
+						for ( var j = nq_key_arr.length; j--; ) {
+
+							if ( item[ nq_key_arr[ j ] ] === rq.q[ nq_key_arr[ j ] ] ) {
+
+								match_bool = false;
+
+							};
+
+						};
+
+						if ( match_bool === true ) {
+
+							match_arr.push( rq.arr[ i ] );
+							
+						};
+					
+					};
+
+					return match_arr;
+
+				};
+
+			},
+
+			extend: function ( obj_1, obj_2 ) {
+
+				Object.keys( obj_2 ).forEach( function ( key ) {
+
+					obj_1[ key ] = obj_2[ key ];
+
+				});
+
+				return obj_1;
+
+			},
+
+			filter: function ( obj ) {
+
+				var keys = Object.keys( obj );
+
+				for ( var i = keys.length; i--; ) {
+
+					if ( !obj[ keys[ i ] ] ) {
+
+						delete obj[ keys[ i ] ];
+
+					};
+
+				};
+
+				return obj;
+
 			},
 
 		};
