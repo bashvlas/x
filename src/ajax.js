f2bb573ce0315d30f28477b05d3e99f21cfff26c
vlas <bashvlas@gmail.com>
2018-08-10 12:03:40 +0300

    backup

--- src/ajax.js
+++ src/ajax.js
@@ -402,3 +402,199 @@
 		return ajax;
 
 	} () );
+
+	window[ window.webextension_library_name ].ajax2 = ( function () {
+
+		var x = window[ window.webextension_library_name ];
+
+		function ajax ( rq ) {
+
+			return new Promise( function ( resolve ) {
+
+				// define event listeners
+
+					function readystatechange_listener () {
+
+						if ( this.readyState === 4 ) {
+
+							if ( this.status === 200 ) {
+
+								resolve({
+
+									meta: {
+
+										error: false,
+										status: this.status,
+
+									},
+
+									data: this.response
+
+								});
+
+							} else {
+
+								resolve({
+
+									meta: {
+
+										error: true,
+										status: this.status,
+										
+									},
+									data: this.response,
+
+								});
+
+							};
+
+						};
+
+					};
+
+					function timeout_listener () {
+
+						resolve({
+
+							meta: {
+
+								error: true,
+								
+							},
+							data: null,
+
+						});
+
+					};
+
+					function load_listener () {
+
+						if ( this.status === 200 ) {
+
+							resolve({
+
+								meta: {
+
+									error: false,
+									status: this.status,
+
+								},
+								data: this.response,
+
+							});
+
+						} else {
+
+							resolve({
+
+								status: this.status,
+								error: true,
+
+							});
+
+						}
+
+					};
+
+					function error_listener () {
+
+						resolve({
+
+							status: this.status,
+							error: true,
+
+						});
+
+					};
+
+				// crate payload
+
+					if ( rq.payload ) {
+
+						if ( rq.payload.type === "query_string" ) {
+
+							var query_string = "";
+
+							Object.keys( rq.payload.data ).forEach( ( key ) => {
+
+								query_string += key + "=" + encodeURIComponent( rq.payload.data[ key ] );
+
+							});
+
+							rq.url += "?" + query_string;
+
+						};
+
+					};
+
+				// init
+
+					var xhr = new XMLHttpRequest();
+					xhr.open( rq.method, rq.url, true );
+
+				// set timeout
+
+					if ( rq.timeout ) {
+
+						xhr.timeout = rq.timeout;
+
+					};
+
+				// set response type
+
+					xhr.responseType = rq.response_type || "text";
+
+				// set headers
+
+					if ( rq.headers ) {
+
+						Object.keys( rq.headers ).forEach( function ( key ) { xhr.setRequestHeader( key, rq.headers[ key ] ) } );
+
+					};
+
+					if ( rq.rq_body_type === "json" ) {
+
+						xhr.setRequestHeader( "Content-Type", "application/json" );
+
+					};
+
+				// register listeners
+
+					xhr.addEventListener( "load", load_listener );
+					xhr.addEventListener( "error", error_listener );
+					xhr.addEventListener( "timeout", timeout_listener );
+					// xhr.addEventListener( "readystatechange", readystatechange_listener );
+
+				// define request body
+
+					if ( rq.rq_body_type === "json" ) {
+
+						var rq_body = JSON.stringify( rq.rq_body );
+
+					} else if ( rq.rq_body_type === "form_data" ) {
+
+						var rq_body = new FormData();
+
+						Object.keys( rq.rq_body ).forEach( function ( key ) {
+
+							rq_body.append( key, rq.rq_body[ key ] );
+
+						});
+
+					} else {
+
+						var rq_body = null;
+
+					};
+
+				// send
+
+					xhr.send( rq_body );
+
+			});
+
+		};
+
+		return ajax;
+
+	} () );
